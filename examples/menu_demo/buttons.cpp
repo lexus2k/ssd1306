e337a2b731a527253c05caee05a91bc863ee3169
Aleksei <alexey.dynda@gmail.com>
2017-10-05 14:23:51 -0400

    added menu items example

---
+++ examples/menu_demo/buttons.cpp
@@ -0,0 +1,43 @@
+/*
+    Copyright (C) 2017 Alexey Dynda
+
+    This file is part of SSD1306 library.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "buttons.h"
+
+#include "Arduino.h"
+
+uint8_t getPressedButton(uint8_t analogPin)
+{
+  int buttonValue = analogRead(analogPin);
+  if (buttonValue < 100) {
+    return BUTTON_RIGHT;  
+  }
+  else if (buttonValue < 200) {
+    return BUTTON_UP;
+  }
+  else if (buttonValue < 400){
+    return BUTTON_DOWN;
+  }
+  else if (buttonValue < 600){
+    return BUTTON_LEFT;
+  }
+  else if (buttonValue < 800){
+    return BUTTON_SELECT;
+  }
+  return BUTTON_NONE;
+}
---
+++ examples/menu_demo/menu_demo.ino
@@ -0,0 +1,99 @@
+/*
+    Copyright (C) 2017 Alexey Dynda
+
+    This file is part of SSD1306 library.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+ *   Attiny85 PINS
+ *             ____
+ *   RESET   -|_|  |- 3V
+ *   SCL (3) -|    |- (2)
+ *   SDA (4) -|    |- (1)
+ *   GND     -|____|- (0) - BUTTONS module
+ *
+ *   Atmega328 PINS: connect LCD to A4/A5,
+ *   Z-keypad ADC module on A0 pin.
+ */
+
+#include "ssd1306.h"
+#include "ssd1306_i2c_conf.h"
+#include "buttons.h"
+
+/* Do not include wire.h for Attiny controllers */
+#ifndef SSD1306_EMBEDDED_I2C
+    #include <Wire.h>
+#endif
+
+#define BUTTON_PIN     A0
+
+/* Define menu items of the menu box */
+const char *menuItems[] =
+{
+    "menu item 1",
+    "menu item 2",
+    "menu item 3",
+    "menu item 4",
+    "menu item 5",
+    "menu item 6",
+    "menu item 7",
+    "menu item 8",
+};
+
+/* This variable will hold menu state, processed by SSD1306 API functions */
+SAppMenu menu;
+
+
+void setup()
+{
+    /* Do not init Wire library for Attiny controllers */
+#ifndef SSD1306_EMBEDDED_I2C
+    Wire.begin();
+    Wire.setClock(400000);
+#endif
+    ssd1306_128x64_i2c_init();
+    ssd1306_fillScreen( 0x00 );
+    /* Initialize main menu state */
+    ssd1306_createMenu( &menu, menuItems, sizeof(menuItems) / sizeof(char *) );
+    /* show menu on the display */
+    ssd1306_showMenu( &menu );
+}
+
+
+void loop()
+{
+    switch (getPressedButton(BUTTON_PIN))
+    {
+        case BUTTON_UP:
+            /* move menu cursor up and refresh menu on the display */
+            ssd1306_menuUp( &menu );
+            ssd1306_updateMenu( &menu );
+            break;
+        case BUTTON_DOWN:
+            /* move menu cursor down and refresh menu on the display */
+            ssd1306_menuDown( &menu );
+            ssd1306_updateMenu( &menu );
+            break;
+        case BUTTON_SELECT:
+            /* You always can request current position of menu cursor, by calling ssd1306_menuSelection() */
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+
