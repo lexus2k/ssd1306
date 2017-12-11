3ffd58a3b783fbc111e9b9ffef3b52408572cecd
Aleksei <alexey.dynda@gmail.com>
2017-12-11 22:52:01 +1000

    Documentation is updated

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['char_5ff6x8',['char_f6x8',['../class_nano_canvas.html#afe659b5c2c540b8af0d8c0fd53eb7327',1,'NanoCanvas']]],
-  ['charf12x16',['charF12x16',['../class_tiny_s_s_d1306.html#af6428ec8ea36aab687f68844042ea828',1,'TinySSD1306']]],
+  ['charf12x16',['charF12x16',['../class_nano_canvas.html#aa1ab1e5c598b6d46f261f45396ea2743',1,'NanoCanvas::charF12x16()'],['../class_tiny_s_s_d1306.html#af6428ec8ea36aab687f68844042ea828',1,'TinySSD1306::charF12x16()']]],
   ['charf6x8',['charF6x8',['../class_nano_canvas.html#a025db957c4a51f9de6afe6b413440d0e',1,'NanoCanvas::charF6x8()'],['../class_tiny_s_s_d1306.html#af7150d11b77af63b73d9a4ab1dda9df4',1,'TinySSD1306::charF6x8()']]],
   ['charf6x8_5feol',['charF6x8_eol',['../class_tiny_s_s_d1306.html#af07d4ee35c4812e5dab3ca453eb1529a',1,'TinySSD1306']]],
   ['clear',['clear',['../class_nano_canvas.html#a6fe2036b269cc55d9181c727a4fb3951',1,'NanoCanvas::clear()'],['../class_sprite_pool.html#a225cec38d6557f304d2279005a8aa523',1,'SpritePool::clear()'],['../class_tiny_s_s_d1306.html#ae9c880933b876e0182a712348febb923',1,'TinySSD1306::clear()']]],
--- docs/search/variables_6.js
+++ docs/search/variables_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['m_5fcanvas',['m_canvas',['../class_sprite_pool.html#aacf3c183566ace3d39ace8755a2a60aa',1,'SpritePool']]],
-  ['m_5frect',['m_rect',['../class_sprite_pool.html#a4a3cbbd6bbd5c9c74f1ed5dd8e631589',1,'SpritePool']]]
+  ['m_5frect',['m_rect',['../class_sprite_pool.html#a4a3cbbd6bbd5c9c74f1ed5dd8e631589',1,'SpritePool']]],
+  ['max_5fsprites',['MAX_SPRITES',['../class_sprite_pool.html#aa7522b59d079d63cfbb0612aaaee58df',1,'SpritePool']]]
 ];
