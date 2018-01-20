fc2b385a4df74b588e886dd3a4abbdcd0187bce6
drgallaci <35565192+drgallaci@users.noreply.github.com>
2018-01-20 02:02:10 +0100

    fillRect() x/y inconsistency fix  @nano_gfx.cpp
    Fix for drawing filled rectangle 1 pixel thinner than specified

--- src/nano_gfx.cpp
+++ src/nano_gfx.cpp
@@ -99,7 +99,7 @@ void NanoCanvas::fillRect(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_
         {
             mask = (mask >> (7 - (y2 & 7)));
         }
-        for (uint8_t x=x1; x<x2; x++)
+        for (uint8_t x=x1; x<=x2; x++)
         {
             m_bytes[BADDR(bank) + x] &= ~mask;
             m_bytes[BADDR(bank) + x] |= (templ & mask);
