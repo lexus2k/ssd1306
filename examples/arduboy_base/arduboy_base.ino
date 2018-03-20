95db201bcb6835e584ebe464b1d37107ea78374c
Aleksei Dynda <alexey.dynda@gmail.com>
2018-03-20 20:21:09 +1000

    Added example for Arduboy hardware

---
+++ examples/arduboy_base/arduboy_base.ino
@@ -0,0 +1,69 @@
+/*
+    MIT License
+
+    Copyright (c) 2018, Alexey Dynda
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+///////////////////////////////////////////////////////////////////////
+// This sketch will run on Arduboy HW (compile for Leonardo board)
+//////////////////////////////////////////////////////////////////////
+
+#include "ssd1306.h"
+#include "nano_engine.h"
+
+NanoEngine1 engine;
+
+NanoRect rect = { {15,12}, {60,35} }; // Lets make rect smaller than in previous example
+
+bool drawAll()
+{
+    engine.canvas.clear();
+    engine.canvas.setColor(RGB_COLOR8(255,255,0));
+    engine.canvas.drawRect(rect);   // draw rect in buffer
+    return true;
+}
+
+void setup()
+{
+    /* Init Mono OLED 128x64 for Arduboy. 6 - RESET, 12 - CS, 4 - D/C */
+    ssd1306_128x64_spi_init(6, 12, 4);
+
+    engine.begin();
+    engine.setFrameRate(45);
+    engine.drawCallback( drawAll );  // Set callback to draw parts, when NanoEngine8 asks
+    engine.connectArduboyKeys();        // Connect ADC-buttons Z-keypad to analog A0 pin
+    engine.refresh();                // Makes engine to refresh whole display content at start-up
+}
+
+void loop()
+{
+    if (!engine.nextFrame()) return;
+    NanoPoint point = {0,0};
+    if (engine.pressed( BUTTON_RIGHT )) point.x = +1;
+    if (engine.pressed( BUTTON_LEFT ))  point.x = -1;
+    if (engine.pressed( BUTTON_UP ))    point.y = -1;
+    if (engine.pressed( BUTTON_DOWN ))  point.y = +1;
+    engine.refresh(rect);            // Update screen content at old rect position
+    rect += point;                   // Move rect according to pressed keys
+    engine.refresh(rect);            // Update screen content at new rect position
+    engine.display();                // refresh display content
+}
+
