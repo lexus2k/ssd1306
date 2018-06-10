6c3e59882f179bbfefc5a7e1fe11504ec92a65e5
Aleksei Dynda <alexey.dynda@gmail.com>
2018-06-10 16:30:05 +1000

    Removed not needed files from documentation

--- docs/search/all_17.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['x',['x',['../struct___nano_point.html#aa846d48822d0fc30348ab60194f0a04c',1,'_NanoPoint::x()'],['../struct_s_p_r_i_t_e.html#a44b2c947f1c6e30f31a77b8520855841',1,'SPRITE::x()']]]
-];
--- docs/search/defines_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['uart_5fbuffer_5frx',['UART_BUFFER_RX',['../ssd1306__uart_8h.html#adff6f1691b8119f8c50293135a28e1b3',1,'ssd1306_uart.h']]]
-];
