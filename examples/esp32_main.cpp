b9dc723333e8801d27a3d568ecb8c5c8e40c6cdd
Aleksei Dynda <alexey.dynda@gmail.com>
2018-06-12 14:19:18 +1000

    Added main task for FreeRTOS

--- examples/esp32_main.cpp
+++ examples/esp32_main.cpp
@@ -22,6 +22,9 @@
     SOFTWARE.
 */
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #ifdef SDL_EMULATION
 #include "sdl_core.h"
 #endif
@@ -29,16 +32,24 @@
 extern void setup(void);
 extern void loop(void);
 
-extern "C" {
-void app_main(void)
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+void main_task(void *args)
 {
     setup();
-    for(;;)
-    {
+    for(;;) {
         loop();
 #ifdef SDL_EMULATION
         sdl_read_analog(0);
 #endif
     }
 }
+
+extern "C" void app_main(void)
+{
+    xTaskCreatePinnedToCore(main_task, "mainTask", 8192, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
 }
