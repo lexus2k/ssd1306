6d6f22e32d82dfac8c5a8585b61b025214246a22
User <alexey.dynda@gmail.com>
2018-06-13 11:34:04 +1000

    Added

---
+++ configure.mk
@@ -0,0 +1,9 @@
+COMPONENT_ADD_INCLUDEDIRS := ./src
+COMPONENT_SRCDIRS := ./src/ssd1306_hal \
+                     ./src/ssd1306_hal/esp \
+                     ./src/intf \
+                     ./src/intf/i2c \
+                     ./src/intf/spi \
+                     ./src/lcd \
+                     ./src/nano_engine
+
