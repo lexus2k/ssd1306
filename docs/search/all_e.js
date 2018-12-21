0af1b98ddaf8aa332919dc7d5bf621a2a4cbb394
lexus2k <alexey.dynda@gmail.com>
2018-12-21 16:22:49 +1000

    Documentation is updated

--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -10,12 +10,12 @@ var searchData=
   ['pos',['pos',['../class_nano_fixed_sprite.html#a515f0fc8500870939edaa86e3e0bc4b4',1,'NanoFixedSprite']]],
   ['pressed',['pressed',['../class_nano_engine_inputs.html#aaf992076a4c0e1a5aa95a9e129e8ad25',1,'NanoEngineInputs']]],
   ['primary_5ftable',['primary_table',['../struct_s_fixed_font_info.html#a8b47480d4d3e59a40411045a1c82730c',1,'SFixedFontInfo']]],
-  ['print',['Print',['../class_print.html',1,'Print'],['../class_print.html#a1b9fe938883bb7b4bce8fba012dab112',1,'Print::Print()'],['../class_print.html#a2131ef6aa11c8551831201ba64cf5f06',1,'Print::print(const char *str)']]],
+  ['print',['Print',['../class_print.html',1,'Print'],['../class_print.html#a1b9fe938883bb7b4bce8fba012dab112',1,'Print::Print()'],['../class_print.html#a2131ef6aa11c8551831201ba64cf5f06',1,'Print::print(const char *str)'],['../class_print.html#a6448e13c050d2ea61ded3c2fca262924',1,'Print::print(int n)']]],
   ['print_5finternal_2eh',['Print_internal.h',['../_print__internal_8h.html',1,'']]],
   ['printchar',['printChar',['../class_nano_canvas_ops.html#a4031b35e730a08985d766d32f7c164fc',1,'NanoCanvasOps']]],
   ['printfixed',['printFixed',['../class_nano_canvas_ops.html#a1174380635ea8d9de6b363e6384f7628',1,'NanoCanvasOps::printFixed()'],['../class_nano_canvas.html#a6f89c3edb9833b7d79f7d1f771b44254',1,'NanoCanvas::printFixed()']]],
   ['printfixed2x',['printFixed2x',['../class_nano_canvas.html#a6dcb23724a60a84baaa06c189903edef',1,'NanoCanvas']]],
   ['printfixedpgm',['printFixedPgm',['../class_nano_canvas_ops.html#adb1263d29d204157c1e87aa5172880b3',1,'NanoCanvasOps']]],
-  ['println',['println',['../class_print.html#a4ce298d7653c5279c7d501222cc8a0a2',1,'Print']]],
+  ['println',['println',['../class_print.html#a4ce298d7653c5279c7d501222cc8a0a2',1,'Print::println(const char *str)'],['../class_print.html#a852fb4084103a6a450b73e5220f233a2',1,'Print::println(int data)']]],
   ['putpixel',['putPixel',['../class_nano_canvas_ops.html#a3f89aff0fb6346cf67799d8695e2f535',1,'NanoCanvasOps::putPixel(lcdint_t x, lcdint_t y)'],['../class_nano_canvas_ops.html#a83157f7cd82e008a5355644dc8deb4ae',1,'NanoCanvasOps::putPixel(const NanoPoint &amp;p)'],['../class_nano_canvas.html#a54a4d7fccec6cd25aa2651301e787241',1,'NanoCanvas::putPixel()']]]
 ];
--- docs/search/files_4.js
+++ docs/search/files_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['lcd_5fcommon_2eh',['lcd_common.h',['../lcd__common_8h.html',1,'']]],
   ['lcd_5fil9163_2eh',['lcd_il9163.h',['../lcd__il9163_8h.html',1,'']]],
+  ['lcd_5fili9341_2eh',['lcd_ili9341.h',['../lcd__ili9341_8h.html',1,'']]],
   ['lcd_5fpcd8544_2eh',['lcd_pcd8544.h',['../lcd__pcd8544_8h.html',1,'']]]
 ];
