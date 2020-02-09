f14ef7903891c7defbc2edab6ceeac88f17d793f
Aleksei Dynda <alexey.dynda@gmail.com>
2020-02-09 18:05:20 +1000

    Released 1.8.2

--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -3,6 +3,7 @@ var searchData=
   ['il9163_5f128x128_5finit',['il9163_128x128_init',['../group___l_c_d___i_n_t_e_r_f_a_c_e___a_p_i.html#gaab951faf7291d6c038296863c3d3b05b',1,'lcd_il9163.c']]],
   ['il9163_5f128x128_5fspi_5finit',['il9163_128x128_spi_init',['../group___l_c_d___i_n_t_e_r_f_a_c_e___a_p_i.html#gaf487eab5aee04c6ce2eca11833992132',1,'lcd_il9163.c']]],
   ['il9163_5fsetmode',['il9163_setMode',['../group___l_c_d___i_n_t_e_r_f_a_c_e___a_p_i.html#gaa85db34b703ff8b54d86a8eea5b30bab',1,'il9163_setMode(lcd_mode_t mode):&#160;lcd_il9163.c'],['../group___l_c_d___i_n_t_e_r_f_a_c_e___a_p_i.html#gaa85db34b703ff8b54d86a8eea5b30bab',1,'il9163_setMode(lcd_mode_t mode):&#160;lcd_il9163.c']]],
+  ['il9163_5fsetoffset',['il9163_setOffset',['../group___l_c_d___i_n_t_e_r_f_a_c_e___a_p_i.html#ga10928d9f89617a3b92731f001283709b',1,'lcd_il9163.c']]],
   ['il9163_5fsetrotation',['il9163_setRotation',['../group___i_l9163___s_t7734___a_p_i.html#ga9810641c687840a775089f19449499c6',1,'il9163_setRotation(uint8_t rotation):&#160;lcd_il9163.c'],['../group___i_l9163___s_t7734___a_p_i.html#ga9810641c687840a775089f19449499c6',1,'il9163_setRotation(uint8_t rotation):&#160;lcd_il9163.c']]],
   ['il9163_2fst7735_3a_20il9163_2fst7735_20control_20functions',['IL9163/ST7735: il9163/st7735 control functions',['../group___i_l9163___s_t7734___a_p_i.html',1,'']]],
   ['ili9341_5f240x320_5finit',['ili9341_240x320_init',['../group___l_c_d___i_n_t_e_r_f_a_c_e___a_p_i.html#ga02c6aa375576491c9d1574f7b6250ff9',1,'lcd_ili9341.c']]],
